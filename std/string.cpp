379a829829211f4de4c73748167458dd34fa46dd
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-04-27 15:21:07 +0200

    Caps change functions

--- std/string.cpp
+++ std/string.cpp
@@ -3,3 +3,13 @@
 void EraseChar(std::string& EraseFrom, char remove) {
 	EraseFrom.erase(std::remove(EraseFrom.begin(), EraseFrom.end(), remove), EraseFrom.end());
 }
+
+#include <string>
+std::string StringToUpper(std::string& convert) {
+	std::transform(convert.begin(), convert.end(), convert.begin(), ::toupper);
+	return convert;
+}
+std::string StringToLower(std::string& convert) {
+	std::transform(convert.begin(), convert.end(), convert.begin(), ::tolower);
+	return convert;
+}
