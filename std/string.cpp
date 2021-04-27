5181c428a1dfe522e0971fa494f4cf9749546568
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-04-28 00:29:31 +0200

    Update string.cpp

--- std/string.cpp
+++ std/string.cpp
@@ -1,15 +1,15 @@
 #include <string>
 #include <algorithm>
 void EraseChar(std::string& EraseFrom, char remove) {
-	EraseFrom.erase(std::remove(EraseFrom.begin(), EraseFrom.end(), remove), EraseFrom.end());
+    EraseFrom.erase(std::remove(EraseFrom.begin(), EraseFrom.end(), remove), EraseFrom.end());
 }
 
 #include <string>
 std::string StringToUpper(std::string& convert) {
-	std::transform(convert.begin(), convert.end(), convert.begin(), ::toupper);
-	return convert;
+    std::transform(convert.begin(), convert.end(), convert.begin(), ::toupper);
+    return convert;
 }
 std::string StringToLower(std::string& convert) {
-	std::transform(convert.begin(), convert.end(), convert.begin(), ::tolower);
-	return convert;
+    std::transform(convert.begin(), convert.end(), convert.begin(), ::tolower);
+    return convert;
 }
